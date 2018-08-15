e74db6df6e82e89f6e2ed94c098ad76464f2a075
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-15 00:24:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift()
+}
+
+function appendDriver(name) {
+  return [...drivers, name];
+}
+
+function prependDriver(name) {
+  return [name, ...drivers];
+}
+
+function removeLastDriver() {
+  let newArray = [...drivers];
+  newArray.pop();
+  return newArray
+}
+
+function removeFirstDriver() {
+  let newArray = [...drivers];
+  newArray.shift();
+  return newArray
+}
